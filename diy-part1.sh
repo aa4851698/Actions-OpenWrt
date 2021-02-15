5057a911337e915fe23b256c861fa9b6759cdbe6
aa4851698 <sgwapk@163.com>
2021-02-15 19:28:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
